dae8f1817bae0e2a0ed112fd9f239b673fe5349f
Raffaela Castro <raffacst@outlook.com>
2024-04-01 14:49:49 -0300

    feat: :lipstick: Add colors constants file
    Add colors constants file
    
    Add colors constants file

---
+++ lib/src/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blueSystem = Color(0xFF1921D9);
+  static const Color blackSystem = Color(0xFF000000);
+  static const Color greySystem = Color(0xFFD9D9D9);
+  static const Color whiteSystem = Color(0xFFFFFFFF);
+}
