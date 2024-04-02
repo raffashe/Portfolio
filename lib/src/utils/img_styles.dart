d039cebccd2a0146d148715e512290902c256efd
Raffaela Castro <raffacst@outlook.com>
2024-04-02 00:14:16 -0300

    feat: :zap: add img profile
    add img profile
    
    add img profile

--- lib/src/utils/img_styles.dart
+++ lib/src/utils/img_styles.dart
@@ -4,20 +4,20 @@ class RaffasheStyles {
   static BoxDecoration imageDecoration() {
     return BoxDecoration(
       image: const DecorationImage(
-        image: AssetImage("assets/images/background.jpg"),
+        image: AssetImage("assets/images/img.png"),
         fit: BoxFit.fill,
       ),
       shape: BoxShape.rectangle,
       borderRadius: BorderRadius.circular(31),
       boxShadow: const [
         BoxShadow(
-          color: Color(0x3F000000),
+          color: Color.fromARGB(62, 128, 128, 128),
           blurRadius: 14,
           offset: Offset(0, 4),
           spreadRadius: 0,
         ),
         BoxShadow(
-          color: Color(0x3F000000),
+          color: Color.fromARGB(62, 126, 126, 126),
           blurRadius: 16,
           offset: Offset(0, -7),
           spreadRadius: 0,
